f77faebb0ac97200ed627816b8f326f720425f5f
Pavlenko Andrii <pavlenkoandrii08@gmail.com>
2022-12-11 12:52:38 +0200

    js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// const firstValue = 0.1;
+// const secondValue = 0.2;
+// const resalt = firstValue + secondValue;
+// console.log(resalt);
+
+// const stringValue = "1";
+// const num = Number(stringValue);
+// const numberValue = 2;
+// const resalt2 = num + numberValue;
+// console.log(resalt2);
+
+const memory = 820000;
+const file = 820;
+const quantity = Math.round(memory / file);
+console.log(quantity);
